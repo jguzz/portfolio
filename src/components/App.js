8a09c72db00bff88a87e8e367b3af6b705e63c68
jguzz <joseph.a.guzzardo@gmail.com>
2020-08-13 11:10:13 -0400

    Setting up some basic layout components'

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import styled from "styled-components";
 import Button from "./style/Button";
+import Body from "./style/Body";
+import Nav from "./style/Nav";
 
 const Heading = styled.h1`
   color: ${(props) => props.theme.primary};
@@ -9,8 +11,14 @@ const Heading = styled.h1`
 function App() {
   return (
     <>
-      <Heading>Root</Heading>
-      <Button>Click me</Button>
+      <Wrapper>
+        <Nav></Nav>
+        <Body>
+          <Heading>Root</Heading>
+          <Button>Click me</Button>
+          <Button kind="dark">Click Me</Button>
+        </Body>
+      </Wrapper>
     </>
   );
 }

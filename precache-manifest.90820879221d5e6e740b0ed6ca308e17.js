e6a1f0204d5dc3aaed807709e33d4854fba8d581
jguzz <joseph.a.guzzardo@gmail.com>
2020-08-18 16:22:20 -0400

    Updates

---
+++ precache-manifest.90820879221d5e6e740b0ed6ca308e17.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffe4e6ce874f508dec75d6c953a0b961",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "e0c9ae9b8cc72396b7bb",
+    "url": "/portfolio/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "91bd405f02957aa83ba4",
+    "url": "/portfolio/static/js/2.27941722.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.27941722.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0c9ae9b8cc72396b7bb",
+    "url": "/portfolio/static/js/main.7167b44f.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "81b911e1d5ccb1c73cdc56ed8347c4d4",
+    "url": "/portfolio/static/media/profile.81b911e1.jpeg"
+  }
+]);

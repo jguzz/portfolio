21bb5a11d010e7fd068dde63d1fe0c741a7b8bd0
jguzz <joseph.a.guzzardo@gmail.com>
2020-09-03 14:31:01 -0400

    working on css animations

--- src/components/App.js
+++ src/components/App.js
@@ -12,23 +12,33 @@ import "../config/stylesheets/style.sass";
 // Has the side nav bar, and the routes for what will display in the body on the right.
 function App() {
   return (
-    <>
+    <div className="wrapper">
       {/* What the user sees when the land on the page */}
       <div className="home">
-        Hello, my name is Joseph Guzzardo. <br /> I'm a fullstack developer.
-        <br /> <button>view my work</button>
+        <p>
+          Joseph A Guzzardo <br /> Full Stack Engineer
+        <div className="cursor"></div>
+        </p>
       </div>
       {/* Sticky nav that links to parts of the page */}
-      <div className='nav'></div>
+      <div className="nav">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Portfolio</li>
+          <li>Education</li>
+          <li>Contact</li>
+        </ul>
+      </div>
       {/* About me */}
-      <div className='about'></div>
+      <div className="about"></div>
       {/* Shows websites ive made with videos */}
-      <div className='portfolio'></div>
+      <div className="portfolio"></div>
       {/* Education and publications */}
-      <div className='education'></div>
+      <div className="education"></div>
       {/* Icons for social media, email form for email. */}
-      <div className='contact'></div>
-    </>
+      <div className="contact"></div>
+    </div>
   );
 }
 
